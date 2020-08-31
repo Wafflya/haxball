a58312cf8334496c1098fd21a4fed029569537dc
Your Name <you@example.com>
2020-09-01 02:26:22 +0300

    1.13.3 add model tour... should be very 2carefull

--- haxball_site/tournament/models.py
+++ haxball_site/tournament/models.py
@@ -189,7 +189,7 @@ class TourNumber(models.Model):
 class Match(models.Model):
     league = models.ForeignKey(League, verbose_name='В лиге', related_name='matches_in_league',
                                on_delete=models.CASCADE)
-    numb_tour = models.ForeignKey(TourNumber, verbose_name='Номер тура', related_name='tour_matches', on_delete=models.CASCADE)
+    numb_tour = models.ForeignKey(TourNumber, verbose_name='Номер тура', related_name='tour_matches', on_delete=models.CASCADE, null=True)
     match_date = models.DateField('Дата матча', default=None, blank=True, null=True)
     replay_link = models.URLField('Ссылка на реплей', blank=True)
     inspector = models.ForeignKey(User, verbose_name='Проверил', limit_choices_to={'is_staff': True},
