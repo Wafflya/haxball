86b19edb4df5e601dde91e07f0a4f602b835461d
Marat <Marat@chel.ru>
2020-09-07 16:13:56 +0500

    add button to off-comments in your profile

--- haxball_site/core/forms.py
+++ haxball_site/core/forms.py
@@ -28,7 +28,7 @@ class CommentForm(forms.ModelForm):
 class EditProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ('about', 'born_date', 'avatar', 'city', 'vk', 'discord', 'telegram')
+        fields = ('about', 'born_date', 'avatar', 'city', 'vk', 'discord', 'telegram', 'commentable')
 
 
 class PostForm(forms.ModelForm):
