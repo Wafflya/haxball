f87a872b1adbc820baa5e6ec86138cd7fd888c0c
Marat <Marat@chel.ru>
2020-09-08 14:00:59 +0500

    1.20.7 added IP-adress table

--- haxball_site/core/views.py
+++ haxball_site/core/views.py
@@ -33,7 +33,7 @@ class PostListView(ListView):
         if x_forwarded_for:
             ip_a = x_forwarded_for
         else:
-            ip_a = self.request.META.get('X-Real-IP')
+            ip_a = self.request.META.get('HTTP_X_Real_IP')
 
         try:
             ipp = IPAdress.objects.get(name=self.request.user)
