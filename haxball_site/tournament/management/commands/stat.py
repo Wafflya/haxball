5d1e12dc57e0b07421f524c147a34e0a799095b0
maratDC <marat@dc.ru>
2020-12-10 11:53:35 +0300

    1.5.7.10 adding overall stat_season

--- haxball_site/tournament/management/commands/stat.py
+++ haxball_site/tournament/management/commands/stat.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from ...models import League, TourNumber, Match, Team
+from ...models import League, TourNumber, Match, Team, Player
 
 
 class Command(BaseCommand):
@@ -58,6 +58,7 @@ class Command(BaseCommand):
             for goal in m.match_goal.all():
                 goals[goal.time_min] += 1
 
+        print(league)
         print('stata')
         print(score_red, score_blue)
         print(win_red, draws, win_blue)
@@ -66,9 +67,10 @@ class Command(BaseCommand):
         print('Goals with assits', goals_with_assist)
         print('Goals in timeline')
         print(goals)
-        print(league)
-        #Stat for each team
+        print('')
 
+        """
+        #Stat for each team
         for t in teams:
             score_red = 0
             score_blue = 0
@@ -127,7 +129,52 @@ class Command(BaseCommand):
             print('Распределение голов по ходу матча')
             print(goals)
             print('')
+        """
 
+        players = Player.objects.all()
 
+        dict = {}
+        for p in players:
+            secs_in_match = 0
+            for m in norm_matches:
+                vishel = m.match_substitutions.filter(player_in=p).count()
+                ushel = m.match_substitutions.filter(player_out=p).count()
+                if (p in m.team_home_start.all()) or (p in m.team_guest_start.all()):
+                    if vishel == 0 and ushel == 0:
+                        secs_in_match += 960
+                    elif vishel == 0 and ushel == 1:
+                        sub_out = m.match_substitutions.get(player_out=p)
+                        secs_in_match += sub_out.time_min * 60 + sub_out.time_sec
+                    elif vishel == 1 and ushel == 1:
+                        sub_out = m.match_substitutions.get(player_out=p)
+                        sub_in = m.match_substitutions.get(player_in=p)
+                        secs_in_match += (sub_out.time_min * 60 + sub_out.time_sec) + (
+                                960 - (sub_in.time_min * 60 + sub_in.time_sec))
+                    elif vishel == 1 and ushel == 2:
+                        sub_in = m.match_substitutions.get(player_in=p)
+                        subs_out = m.match_substitutions.filter(player_out=p).order_by('time_min', 'time_sec')
+                        print(subs_out)
+                        secs_in_match += (subs_out[0].time_min * 60 + subs_out[0].time_sec) + (
+                                (subs_out[1].time_min - sub_in.time_min) * 60 + (
+                                    subs_out[1].time_sec - sub_in.time_sec))
+                else:
+                    if vishel == 1 and ushel == 0:
+                        sub_in = m.match_substitutions.get(player_in=p)
+                        secs_in_match += 960 - (sub_in.time_min * 60 + sub_in.time_sec)
+                    elif vishel == 1 and ushel == 1:
+                        sub_out = m.match_substitutions.get(player_out=p)
+                        sub_in = m.match_substitutions.get(player_in=p)
+                        secs_in_match += (
+                                    (sub_out.time_min - sub_in.time_min) * 60 + (sub_out.time_sec - sub_in.time_sec))
+                    elif vishel == 2 and ushel == 1:
+                        subs_in = m.match_substitutions.filter(player_in=p).order_by('time_min', 'time_sec')
+                        sub_out = m.match_substitutions.get(player_out=p)
+                        secs_in_match += ((sub_out.time_min - subs_in[0].time_min) * 60 + (
+                                    sub_out.time_sec - subs_in[0].time_sec)) + (960 - (subs_in[1].time_min*60 + subs_in[1].time_sec))
+            dict[p] = secs_in_match
+        l = sorted(dict, key=lambda x: dict[x], reverse=True)
+        for i in l:
+            print(i, dict[i])
+        print('')
 
         print('The End')
