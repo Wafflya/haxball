3f21b99d3b3a67ac88e54a5d1917591ec7126a13
maratDC <marat@dc.ru>
2020-12-10 09:35:03 +0300

    1.5.7.2 adding overall stat_season

--- haxball_site/tournament/management/commands/stat.py
+++ haxball_site/tournament/management/commands/stat.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from ...models import League, TourNumber, Match
+from ...models import League, TourNumber, Match, Team
 
 
 class Command(BaseCommand):
@@ -12,6 +12,7 @@ class Command(BaseCommand):
         except:
             print('Ошибка выбора лиги')
         matches = Match.objects.filter(league=league, is_played=True)
+        teams = league.teams.all()
         # print(matches)
         norm_matches = []
         tp_matches = []
@@ -20,7 +21,6 @@ class Command(BaseCommand):
 
             goals_scored = m.match_goal.count() + m.match_event.filter(event='OG').count()
 
-            print(all_goals, goals_scored)
             if goals_scored == all_goals:
                 norm_matches.append(m)
             else:
@@ -67,4 +67,50 @@ class Command(BaseCommand):
         print('Goals in timeline')
         print(goals)
         print(league)
+        #Stat for each team
+
+        score_red = 0
+        score_blue = 0
+        win_red = 0
+        win_blue = 0
+        draws = 0
+        goals = [0 for _ in range(16)]
+        # goals - og count
+        goals_not_og = 0
+        goals_with_assist = 0
+        og = 0
+        for t in teams:
+            for m in norm_matches:
+                if m.team_home == t or m.team_guest == t:
+                    print(m)
+                    goals_not_og += m.match_goal.count()
+                    for g in m.match_goal.all():
+                        if g.assistent:
+                            goals_with_assist += 1
+                    og += m.match_event.filter(event='OG').count()
+
+                    score_red += m.score_home
+                    score_blue += m.score_guest
+                    if m.score_home > m.score_guest:
+                        win_red += 1
+                    elif m.score_home < m.score_guest:
+                        win_blue += 1
+                    else:
+                        draws += 1
+
+                    for goal in m.match_goal.all():
+                        goals[goal.time_min] += 1
+
+            print('Статистика ', t)
+            print(score_red, score_blue)
+            print(win_red, draws, win_blue)
+            print('OG', og)
+            print('Goals not og', goals_not_og)
+            print('Goals with assits', goals_with_assist)
+            print('Goals in timeline')
+            print(goals)
+            print('')
+
+
+
         print('The End')
