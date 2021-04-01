f27070d05cf4e5b4ff6cf343fe7427de71e625ee
Marat home <vmarat95@gmail.com>
2021-04-01 22:55:26 +0300

    1.8.0.12 add link in navbar

--- haxball_site/core/views.py
+++ haxball_site/core/views.py
@@ -205,8 +205,10 @@ class AdminListView(ListView):
     a = []
     for i in us:
         a.append([i, len(i.get_user_permissions()), i.id])
-    b = a.sort(key=lambda x: x[2])
-    c = b.sort(key=lambda x: x[1], reverse=True)
+    print(a)
+    b = sorted(a, key=lambda x: x[2])
+    print(b)
+    c = sorted(b, key=lambda x: x[1], reverse=True)
     queryset = [i[0] for i in c]
     context_object_name = 'users'
     template_name = 'core/admins/admin_list.html'
