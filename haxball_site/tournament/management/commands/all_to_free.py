382b5b20a33c663cc5d8b974662c2cf620435430
Wafflya <vmarat95@gmail.com>
2021-09-01 13:25:01 +0300

    add some goodneses

--- haxball_site/tournament/management/commands/all_to_free.py
+++ haxball_site/tournament/management/commands/all_to_free.py
@@ -1,7 +1,9 @@
+from datetime import timezone
+
 from django.core.management.base import BaseCommand, CommandError
 from django.db.models import Q
 
-from ...models import League, TourNumber, Match, Season, Player
+from ...models import League, TourNumber, Match, Season, Player, PlayerTransfer
 
 
 class Command(BaseCommand):
@@ -21,6 +23,10 @@ class Command(BaseCommand):
         players_in_team = Player.objects.filter(~Q(team=None))
 
         print('Выборка по сезону {}'.format(options['champ_number']))
+
         for p in players_in_team:
             print(p)
-
+        s = players_in_team.first()
+        print(s, s.team)
+        PlayerTransfer.objects.create(trans_player=s, to_team=None, season_join=seas, date_join=timezone.now)
+        print(s, s.team)
