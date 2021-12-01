7af3209cc6e32c37376210afa78deecd9ae75c4e
Marat <42880464+Wafflya@users.noreply.github.com>
2021-12-01 10:54:17 +0300

    Update tournament_extras.py

--- haxball_site/tournament/templatetags/tournament_extras.py
+++ haxball_site/tournament/templatetags/tournament_extras.py
@@ -158,7 +158,7 @@ def player_team(player):
     for s in seasons:
         d2 = {}
         trans_teams = list(
-            PlayerTransfer.objects.filter(~Q(to_team=None), season_join=s, trans_player=player.user_player)).distinct('to_team')
+            PlayerTransfer.objects.filter(~Q(to_team=None), season_join=s, trans_player=player.user_player).distinct('to_team'))
         for team in trans_teams:
             d3 = {}
             leagues = list(League.objects.filter(championship=s, teams=team.to_team).order_by('id'))
