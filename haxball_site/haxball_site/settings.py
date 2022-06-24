271892af070fe4d2664a692bf8c43a73d42e83f9
Marat <vmarat95@gmail.com>
2022-06-25 02:12:38 +0300

    update gmail pass

--- haxball_site/haxball_site/settings.py
+++ haxball_site/haxball_site/settings.py
@@ -159,8 +159,6 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
-
 if DEBUG:
     # EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
@@ -189,14 +187,11 @@ else:
         }
     }
 
-
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'haxballcis@gmail.com'
-EMAIL_HOST_PASSWORD = 'ebalvrotmarokko32288'
-
-
+EMAIL_HOST_PASSWORD = 'zdopbikwvtwwinyf'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
